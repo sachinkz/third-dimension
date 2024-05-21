954791455283f8d59d23dc463f8ee9730485cfdf
sachin-kx <oggypubgaming@gmail.com>
2024-05-21 17:26:56 +0530

    edited fileupload

--- middleware/fileUpload.js
+++ middleware/fileUpload.js
@@ -5,7 +5,7 @@ import { v4 as uuid } from 'uuid'
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads/');
+        cb(null, 'uploads');
     },
     filename: function (req, file, cb) {
         cb(null, uuid() + path.extname(file.originalname));
